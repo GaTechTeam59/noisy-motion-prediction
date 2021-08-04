d55045ceb46886332783af20361844fcff6bef87
AdamsonMA <adamsonma@g.cofc.edu>
2021-08-03 22:13:53 -0400

    Update add_overlay.py

--- overlays/add_overlay.py
+++ overlays/add_overlay.py
@@ -1,18 +1,20 @@
 import cv2 #Requires opencv-contrib-python==4.1.2.30
 import os
 import random
+import numpy as np
 
-def add_overlay(filename,overlay_type, opacity=1, blur=0, write=False):
+def add_overlay(foldername,filename,overlay_type, opacity=1, blur=0, write=False):
 	#Load videos
-	vidcap_sample = cv2.VideoCapture("samples/"+filename)
+	vidcap_sample = cv2.VideoCapture("serre/hmdb51_org/"+foldername+"/"+foldername+"/"+filename);
 	vidcap_overlay = cv2.VideoCapture("overlays/overlay_"+overlay_type+".mp4")
+
 	
 	success_sample,image_sample = vidcap_sample.read()
 	success_overlay,image_overlay = vidcap_overlay.read()
 	
 	#Save video if used for testing purposes.
 	outshape = (image_sample.shape[1], image_sample.shape[0])
-	if write: out_video = cv2.VideoWriter("output_videos/output_"+filename, cv2.VideoWriter_fourcc(*'mp4v'), 30, outshape)
+	if write: out_video = cv2.VideoWriter("hmdb_overlay/"+overlay_type+"/"+foldername+"/"+filename, cv2.VideoWriter_fourcc('F', 'M', 'P', '4'), 30, outshape)
 	
 	#This selects a random overlay frame to start with.
 	skip_frames = random.randint(0,30);
@@ -32,9 +34,6 @@ def add_overlay(filename,overlay_type, opacity=1, blur=0, write=False):
 			#Opacity over 1 amplifies the overlay.
 			new_img = cv2.addWeighted(image_sample,1,image_overlay,opacity,0)
 			
-			#Example of how gaussian blur is applied.  the blur size must be an odd integer.
-			if blur > 0: new_img = cv2.GaussianBlur(new_img,(blur,blur),cv2.BORDER_DEFAULT)
-
 			if write: out_video.write(new_img)
 			
 			#Iterate to the next frame.
@@ -47,16 +46,20 @@ def add_overlay(filename,overlay_type, opacity=1, blur=0, write=False):
 	
 #This creates a test video fore each of the six overlays and a 7th that creates a gaussian blur.
 def test_overlays(show=False):
-	files = os.listdir("samples");
-	overlays = ["lights","snow","snow","fog","rain","fire","grunge"]
-	opacities = [0.5,0,1,1,1,5,1]
-	blurs = [0,15,0,0,0,0,0]
-	
-	for i in range(len(files)):
-		if show: print(files[i]);
-		add_overlay(files[i],overlays[i],opacity=opacities[i],blur=blurs[i],write=True);
-	
-	
-test_overlays(show=True);
+	folders = ['cartwheel', 'climb', 'dive', 'kick', 'pullup', 'run', 'sit', 'situp', 'somersault', 'stand']
+	for f in range(len(folders)):
+		print("FOLDER:",folders[f])
+		files = os.listdir("serre/hmdb51_org/"+folders[f]+"/"+folders[f])
+		overlays = ["lights","snow","fog","rain","fire","grunge"]
+		
+		opacity_multipliers = [0.25,0.5,0.5,0.5,4,0.4]#Some overlays are stronger than others.
+		
+		#Iterate every file
+		for i in range(len(files)):
+			if show: print(len(files)-i,"\t",files[i]);
+			if ".avi" in files[i] or ".mp4" in files[i]:
+				for j in range(len(overlays)):
+					this_opacity = random.random() * opacity_multipliers[j]+0.25;
+					add_overlay(folders[f],files[i],overlays[j],opacity=this_opacity,write=True);
+test_overlays();			
 
-	
